f07a8b4c889eb680e155c0345ba264be4afceaae
Stanyke <chibuike.stanley50@gmail.com>
2020-02-12 15:20:03 +0100

    Adding filter buttons

--- src/components/resturants.js
+++ src/components/resturants.js
@@ -9,6 +9,8 @@ class AllOpenResturants extends Component{
         super(props);
         this.state = {
             resturant:[],
+            DayOfWeek:"",
+            HourOfDay:"",
             erroMsg:""
        }
       }
@@ -34,10 +36,64 @@ class AllOpenResturants extends Component{
         return (
           <div class="container">
 
-              <h2>Resturant</h2>
-            <div class="row">
+              <h2>Restaurant</h2>
 
+            <div class="row">
+              <div class="col-5">
+                <select id="DayOfWeek" class="form-control" onChange={(value)=> this.setState({DayOfWeek:value.target.value})}>
+                  <option value="">Choose Day Of Week</option>
+                  <option value="sunday">Sunday</option>
+                  <option value="monday">Monday</option>
+                  <option value="tuesday">Tuesday</option>
+                  <option value="wednesday">Wednesday</option>
+                  <option value="thursday">Thursday</option>
+                  <option value="friday">Friday</option>
+                  <option value="saturday">Saturday</option>
+                </select>
+              </div>
               
+              <div class="col-5">
+                <select id="HourOfDay" class="form-control" onChange={(value)=> this.setState({HourOfDay:value.target.value})}>
+                  <option value="">Choose Hour Of The Day</option>
+                  <option value="00">12 AM</option>
+                  <option value="01">01 AM</option>
+                  <option value="02">02 AM</option>
+                  <option value="03">03 AM</option>
+                  <option value="04">04 AM</option>
+                  <option value="05">05 AM</option>
+                  <option value="06">06 AM</option>
+                  <option value="07">07 AM</option>
+                  <option value="08">08 AM</option>
+                  <option value="09">09 AM</option>
+                  <option value="10">10 AM</option>
+                  <option value="11">11 AM</option>
+                  <option value="12">12 PM</option>
+                  <option value="13">01 PM</option>
+                  <option value="14">02 PM</option>
+                  <option value="15">03 PM</option>
+                  <option value="16">04 PM</option>
+                  <option value="17">05 PM</option>
+                  <option value="18">06 PM</option>
+                  <option value="19">07 PM</option>
+                  <option value="20">08 PM</option>
+                  <option value="21">09 PM</option>
+                  <option value="22">10 PM</option>
+                  <option value="23">11 PM</option>
+                </select>
+              </div>
+              <div class="col-2">
+                <button type="submit" class="form-control btn btn-success" onClick={()=>this.filterRestaurant()}>Search</button>
+              </div>
+
+              <br/>
+              <br/>
+
+              <div id="popError" class="col-12"></div>
+
+            </div>
+
+
+            <div class="row">
 
               <br/>
               <br/>
@@ -90,6 +146,20 @@ class AllOpenResturants extends Component{
             </div>
           </div>
         );
+      }
+
+      filterRestaurant()
+      {
+        const errorNotice = document.getElementById('popError');
+
+        if (this.state.DayOfWeek === "")
+        {
+          errorNotice.innerHTML = "<div class='alert alert-danger alert-dismissible fade show'><button type='button' class='close' data-dismiss='alert'>&times;</button><i class='fa fa-male'><strong> Day Of The Week Not Selected</strong></div>";
+        }
+        else if (this.state.HourOfDay === "")
+        {
+          errorNotice.innerHTML = "<div class='alert alert-danger alert-dismissible fade show'><button type='button' class='close' data-dismiss='alert'>&times;</button><i class='fa fa-male'><strong> Hour Of The Day Not Selected</strong></div>";
+        }
       }
     }
 
